e3a21e3a3cb52a6ee7b8194fd15e27cae5c94416
Sakhile Lindah Ndlazi <sakhilelindah@protonmail.com>
2024-01-26 09:32:45 +0200

    Insertsa new document in a collection based on kwargs.

---
+++ 0x01-NoSQL/9-insert_school.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+Insert a new document in a collection based on kwargs
+"""
+import pymongo
+
+
+def insert_school(mongo_collection, **kwargs):
+    """
+    Insert a document into a collection.
+    """
+    data = mongo_collection.insert_one(kwargs)
+    return data.inserted_id
